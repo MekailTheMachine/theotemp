0118a6ca816cd80f02e3d5dbbb9efb5eef23904b
Mekail The Machine <mekailmurphy@gmail.com>
2024-03-19 13:36:10 -0600

    Update Theo tings

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -15,26 +15,26 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">THEO Token</span>
           </h1>
           <div className="flex justify-center items-center space-x-2">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Get started by {" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
+              Aping your entire networth
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Edit your finances in {" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
+              THEO's .sol
             </code>{" "}
-            in{" "}
+            and bet on {" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
+              your future
             </code>
           </p>
         </div>
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Tinker with your Theo contract using the{" "}
                 <Link href="/debug" passHref className="link">
                   Debug Contracts
                 </Link>{" "}
